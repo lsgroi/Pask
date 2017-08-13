b9aaf45a17d5c00d01bfe3195254ac4454523084
Luca Sgroi <lucasgroi@gmail.com>
2017-08-13 21:57:29 +0100

    Update dependencies

--- Pask/tasks/ZipExtract-Artifact/.build/build.ps1
+++ Pask/tasks/ZipExtract-Artifact/.build/build.ps1
@@ -1,4 +1,4 @@
 Import-Task Restore-NuGetPackages, Clean, Build, New-Artifact, Zip-Artifact, Extract-Artifact
 
 # Synopsis: Default task
-Task . Restore-NuGetPackages, Clean, Build, Build, New-Artifact, Zip-Artifact, Extract-Artifact
+Task . Restore-NuGetPackages, Clean, Build, New-Artifact, Zip-Artifact, Extract-Artifact
